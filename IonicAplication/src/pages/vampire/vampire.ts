69cf95bd4871065322902466f4f8bdb9eafcbacc
G-Note <germanomgomes@hotmail.com>
2018-02-26 23:46:49 -0300

    Refactoring and more tests.

--- IonicAplication/src/pages/vampire/vampire.ts
+++ IonicAplication/src/pages/vampire/vampire.ts
@@ -15,6 +15,31 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class VampirePage {
 
+  rights: any = true;
+  middles: any = true;
+  lefts: any = true;
+  
+  talents: any = true;
+  expertises: any = true;
+  knowledges: any = true;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  	this.rights = ['Nome', 'Jogador', 'Crônica'];
+  	this.middles = ['Natureza', 'Comportamento', 'Clã'];
+  	this.lefts = ['Geração', 'Refúgio', 'Conceito'];
+
+  	this.talents = ['Prontidão', 'Esportes', 'Briga',
+  	'Esquiva', 'Empatia', 'Expressão', 'Intimidação',
+  	'Liderança', 'Manha', 'Lábia'];
+  	this.expertises = ['Empatia com animais', 'Ofícios',
+  	'Condução', 'Etiqueta', 'Armas de fogo', 'Armas brancas',
+  	'Perfomance', 'Segurança', 'Furtividade', 'Sobrevivência'];
+  	this.knowledges = ['Acadêmicos', 'Computador', 'Finanças',
+  	'Investigação', 'Direito', 'Linguística', 'Medicina',
+  	'Ocultismo', 'Política', 'Ciência'];
+  }
+
+  showOptions(){
+  	
   }
 }
