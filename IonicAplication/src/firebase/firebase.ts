11d537f66ebf69046a4f2fe7e675f9e38092f9f7
Germano <germanomgomes@hotmail.com>
2018-09-17 23:26:12 -0300

    Remove Firebase credentials
    No need to expose our firebase credentials

--- IonicAplication/src/firebase/firebase.ts
+++
@@ -1,8 +0,0 @@
-export const firebase = {
-    apiKey: "AIzaSyDMRaUskC9g0ciMI7Gk-6r3djSNyuAAVHo",
-    authDomain: "tokenmanagersystem.firebaseapp.com",
-    databaseURL: "https://tokenmanagersystem.firebaseio.com",
-    projectId: "tokenmanagersystem",
-    storageBucket: "tokenmanagersystem.appspot.com",
-    messagingSenderId: "831693055476"
-};
